f7a2a2df2c0a17fcdc23c770d0668b03572d75c8
Pedro Palau <pepalauisaac@gmail.com>
2020-03-24 16:28:12 -0500

    Implements a proper way to load json data from a file without errors using `now`
    https://stackoverflow.com/questions/43389724/lambda-function-error-erofs-read-only-file-system-open-tmp-test-zip-proc

--- main.js
+++ main.js
@@ -3,11 +3,18 @@
  * to be used in production environments like `now`
  **/
 
-const jsonServer    = require('json-server')
-const server        = jsonServer.create()
-const router        = jsonServer.router('data.json')
-const middlewares   = jsonServer.defaults()
-const port          = process.env.PORT || 3000
+const fs = require('fs')
+const jsonServer = require('json-server')
+const data = require('./data.json')
+const server = jsonServer.create()
+
+temp = '/tmp/data.json'
+
+fs.writeFileSync(temp, JSON.stringify(data))
+
+const router = jsonServer.router(temp)
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000
 
 server.use(middlewares)
 server.use(router)
